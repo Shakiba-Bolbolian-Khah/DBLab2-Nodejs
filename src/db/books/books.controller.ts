99d853fc530d6a37ffc4f4bd36cdd95c908c8ea4
shakiba-bolbolian-khah <shakiba.bl99@gmail.com>
2021-04-22 01:08:17 +0430

    Part 4 completed - PUT and DELETE book works completely

--- src/db/books/books.controller.ts
+++ src/db/books/books.controller.ts
@@ -1,17 +1,33 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Header, Delete } from '@nestjs/common';
+import {ApiResponse } from '@nestjs/swagger';
 import BooksServices from './books.service';
 import CreateBookDto from '../dto/create-book.dto';
+import EditBookDto from '../dto/edit-book.dto';
 
 @Controller('books')
 export default class BooksController {
   constructor(private readonly bookServices: BooksServices) {}
   @Post('post')
-  postBook( @Body() genre: CreateBookDto) {
-    return this.bookServices.insert(genre);
+  postBook( @Body() book: CreateBookDto) {
+    return this.bookServices.insert(book);
   }
 
   @Get()
   getAll() {
     return this.bookServices.getAllBooks();
+  }
+
+  @Put('update')
+  @ApiResponse({ status:200, description:'Updating selected book' })
+  async update( @Body() book: EditBookDto) {
+    return this.bookServices.update(book);
+  }
+
+
+  @Delete('delete')
+  @ApiResponse({ status:200, description:'Deleting selected book' }) 
+  @Header('Content-Type', 'application/json')
+  async delete(@Body() bookID: number) {
+    return this.bookServices.delete(bookID);
   }
 }
